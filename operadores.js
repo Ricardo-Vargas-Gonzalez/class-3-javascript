645c3dd4b2da3aa26dd4410c7b2ef2e5ac5b90f0
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2023-02-18 13:28:59 -0600

    operadores base

--- operadores.js
+++ operadores.js
@@ -8,17 +8,19 @@ x += y // Es exactamente lo mismo que hace esto: x = x ´y
 x += y
 
 lista.push(x)
-console.log(x);
+console.log(lista);
 
 // asignación de resta
 y -= x
 
-console.log(y)
+lista.push(y);
+console.log(lista);
 
 // Asignación de multiplicación
 x *= y
 
-console.log("x = " + x)
+lista.push(y)
+console.log(lista);
 
 x = 5
 y = 10
@@ -26,14 +28,61 @@ y = 10
 // Operador de bit a bit de Desplazamiento a la izquierda, desplaza 10 lugares, en este caso a la izquierda
 x <<= y
 
-console.log("x = " + x)
+lista.push(x);
+console.log(lista);
 
-x = 5
-y = 11
+x = 5;
+y = 11;
 
 // Asignación de modulo o residuo
-console.log(y % x)
+console.log(y % x);
 
-y % n
+y %= n;
 
-console.log("Y = " + y)
+lista.push(y)
+console.log(lista);
+
+//estructura de control if
+let var1 = 3;
+let var2 = 4;
+
+//operador igual
+if (var1 == var2) {
+    console.log("Son iguales");
+} else {
+    console.log("No son iguales ");
+}
+
+//operador estrictamente igual
+if (var1 === '3') {
+    console.log("Son iguales");
+} else {
+    console.log("No son iguales ");
+}
+
+//Operador de desigualdad
+if (var1 != '4') {
+    console.log("Son diferentes");
+} else {
+    console.log("No son diferentes ");
+}
+
+//Operador de deigualdad estricta
+if (var1 !== '3') {
+    console.log("Son diferentes");
+} else {
+    console.log("No son diferentes  ");
+}
+
+// > Mayor que
+console.log(var1 > var2);
+
+// < Menor que 
+console.log(var1 < var2);
+console.log(23 < 23);
+
+// Menor o Igual que 
+console.log(23 <= 23);
+
+// Mayor o Igual que
+console.log(23 >=  23);
